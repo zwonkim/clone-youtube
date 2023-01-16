6409d039f30f697b2f082f54dfde2f39a4649752
zwonkim <k980912@naver.com>
2023-01-16 17:27:36 +0900

    test

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -16,7 +16,7 @@ export default function Videos() {
   return (
     <>
       {isLoading && <p>Loading...</p>}
-      {error && <p>Something is wrong 😖</p>}
+      {error && <p>Something is wrong</p>}
       {videos && (
         <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
           {videos.map((video) => (
